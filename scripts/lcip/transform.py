02cfa19a08645e48aec9d581524ee1dfdba3b298
Taz <97239140+taz-chiles@users.noreply.github.com>
2024-08-16 12:40:46 +0100

    Automate processing of LCIP data

--- scripts/lcip/transform.py
+++ scripts/lcip/transform.py
@@ -1,11 +1,11 @@
 import os 
 import pandas as pd
+from pathlib import Path
 
 from thefuzz import process
 
 OUT_DIR = os.path.join('src', '_data', 'viz', 'lcip')
-# DATA_DIR = os.path.join('data', 'lcip', 'lcip_grant_stats.xlsx')
-INSPIRE_DATA = os.path.join('data', 'lcip', 'inspire-data.xlsx')
+DATA_DIR = os.path.join('data', 'lcip')
 WARD_DATA = os.path.join('data', 'leeds_wards.csv')
 
 # Function to fuzzy match and merge dataframes
@@ -20,45 +20,43 @@ def fuzzy_merge(df1, df2, key1, key2, threshold=90):
     merged.drop(['matches', 'score'], axis=1, inplace=True)
     return merged
 
+def process_wards(ward_data, data, out_path, theme, output_file):
+    wards_data = data[data['THEME'] == theme]
+    merged_data = (
+        fuzzy_merge(ward_data, wards_data, 'WD21NM', 'METRIC')
+        .dropna()
+        .drop(columns='WD21NM')
+        .rename(columns={'WD21CD': 'ward_code', 'THEME': 'metric', 'METRIC': 'ward_name', 'R1 Q1': 'value'})
+    )
+    merged_data['value'] = merged_data['value'].round(0).astype(int)
+    merged_data.to_csv(os.path.join(out_path, output_file), index=False)
+
 if __name__ == "__main__":
 
-    # Process Inspire data
-
-    inspire_data = pd.read_excel(INSPIRE_DATA).drop(columns={'R2 Q2','R3 Q3','R4 Q4'})
     ward_data = pd.read_csv(WARD_DATA)
 
-    # inspire_data = inspire_data.pivot(index='METRIC', columns='THEME', values='R1 Q1')
+    for filename in os.listdir(DATA_DIR):
+        if filename.endswith(".xlsx") or filename.endswith(".xls"):  
+            file_path = os.path.join(DATA_DIR, filename)
+            filename_stem = Path(filename).stem
+            out_path = os.path.join(OUT_DIR, filename_stem)
+            try:
+                data = pd.read_excel(file_path).drop(columns={'R2 Q2','R3 Q3','R4 Q4'})
+                data['THEME'] = data['THEME'].str.rstrip()
+                data['METRIC'] = data['METRIC'].str.rstrip()
 
-    inspire_data_na = inspire_data.dropna().loc[inspire_data['R1 Q1'] != 0]
-    themes = inspire_data_na['THEME'].unique()
+                data_na = data.dropna().loc[data['R1 Q1'] != 0]
 
-    for theme in themes:
-        theme_df = inspire_data_na[inspire_data_na['THEME'] == theme]
-        theme_df = theme_df.pivot_table(index='THEME', columns='METRIC', values='R1 Q1')
-        theme_filename = theme.replace(" ", "_").replace("/", "_") + '.csv'
-        theme_df.to_csv(os.path.join(OUT_DIR, theme_filename), index=False)
+                themes = data_na['THEME'].unique()
+                for theme in themes:
+                    theme_df = data_na[data_na['THEME'] == theme]
+                    theme_df = theme_df.pivot_table(index='THEME', columns='METRIC', values='R1 Q1')
+                    theme_df = theme_df.round(0).astype(int)
+                    theme_filename = theme.replace(" ", "_").replace("/", "_").replace('(', '').replace(')', '').replace('_-_', '_').lower() + '.csv'
+                    theme_df.to_csv(os.path.join(out_path, theme_filename), index=True)
 
-    wards_applied = inspire_data.loc[inspire_data['THEME']== 'WARDS - APPLICANT BASED']
-    wards_received = inspire_data.loc[inspire_data['THEME']== 'WARDS - RECEIVING ACTIVITY']
+                process_wards(ward_data, data_na, out_path, 'WARDS - APPLICANT BASED', 'applications_by_ward.csv')
+                process_wards(ward_data, data_na, out_path, 'WARDS - RECEIVING ACTIVITY', 'received_by_ward.csv')
 
-    applied = (
-        fuzzy_merge(ward_data, wards_applied, 'WD21NM', 'METRIC')
-        .dropna()
-        .drop(columns='WD21NM')
-        .rename(columns={
-            'WD21CD': 'ward_code',
-            'THEME': 'metric',
-            'METRIC': 'ward_name',
-            'R1 Q1': 'value'
-        })).to_csv(os.path.join(OUT_DIR, 'inspire', 'APPLICATIONS_BY_WARD.csv'), index=False)
-    
-    funded = (
-        fuzzy_merge(ward_data, wards_received, 'WD21NM', 'METRIC')
-        .dropna()
-        .drop(columns='WD21NM')
-        .rename(columns={
-            'WD21CD': 'ward_code',
-            'THEME': 'metric',
-            'METRIC': 'ward_name',
-            'R1 Q1': 'value'
-        })).to_csv(os.path.join(OUT_DIR, 'inspire', 'RECEIVED_BY_WARD.csv'), index=False)
+            except Exception as e:
+                print(f"Error reading LCIP data - {filename}: {e}")
